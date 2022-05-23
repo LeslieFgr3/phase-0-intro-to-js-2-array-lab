5376e9afba16001790c3734878ebdd9291fb9c03
Leslie <lesliefgr3@gmail.com>
2022-05-23 10:23:30 -0400

    commit tree 3fef6daa2e5e621eaed7faaab1f210a440e51ae1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat() {
+    const newCat = [...cats,"Broom"];
+    return newCat;
+}
+
+function prependCat() {
+    const newCat = ["Arnold", ...cats];
+    return newCat;
+}
+
+function removeLastCat() {
+    const newCat = [...cats]
+    newCat.pop();
+    return newCat;
+
+}
+
+function removeFirstCat() {
+    const newCat = [...cats];
+    newCat.shift();
+    return newCat;
+}
